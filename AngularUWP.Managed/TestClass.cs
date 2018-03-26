3d2223a41f6104a0ccac20773a224a4e376f395e
jins <jins@qburst.com>
2018-03-26 17:15:01 +0530

    added Runtime component

---
+++ AngularUWP.Managed/TestClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngularUWP.Managed
+{
+    public sealed class TestClass
+    {
+        public string TestFunction() {
+
+            return "Hello World";
+        }
+    }
+}
